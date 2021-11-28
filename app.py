5a8a2ca887ae7db539b2473ef044ed331692adca
Isaac Ndema <60468539+script-0@users.noreply.github.com>
2021-11-28 10:20:30 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from imblearn.over_sampling import SMOTENC
 from sklearn.model_selection import train_test_split
 
 from sklearn.ensemble import RandomForestClassifier
-
+from joblib import load
 
 app = Flask(__name__)
 CORS(app)
@@ -103,11 +103,6 @@ def predict_biopsy():
 
 @app.route('/mammogram',methods=['POST'])
 def predict_mammogram():
-    global min_age
-    global max_age 
-    global max_bmi
-    global min_bmi
-
     data = request.get_json()
 
     '''
@@ -124,36 +119,8 @@ def predict_mammogram():
 
     '''
 
-    #update min and max age 
-    if (data['age'] > max_age):
-        max_age = data['age']
-        print('Updating max age')
-    elif (data['age'] < min_age):
-        # global min_age
-        min_age = data['age']
-        print('Updating min age')
-    else:
-        pass
-
-    #update min and max bmi
-    if (data['bmi'] > max_bmi):
-        #global max_age
-        print('Updating max bmi from',max_age)
-        max_bmi = data['bmi']
-    elif (data['bmi'] < min_bmi):
-        #global min_bmi
-        min_bmi = data['bmi']
-        print('Updating min bmi')
-    else:
-        pass
-
-    #normalize age
-    #data['age'] = (data['age'] - min_age)/( max_age - min_age)
     data['age'] = data['age']/100.0
-
-    #normalize bmi
-    #data['bmi'] = (data['bmi'] - min_bmi)/( max_bmi - min_bmi)
-    data['bmi'] = data['bmi'/100.0
+    data['bmi'] = data['bmi']/100.0
 
     ar = np.array([[data['age'], 
                     data['bmi'], 
@@ -192,7 +159,6 @@ def predict_mammogram():
         return jsonify({"error":"Bad Request." , "Description":"Bad Method. Only POST is accepted"})
     '''
 
-
 # A welcome message to test our server
 @app.route('/')
 def index():
@@ -202,74 +168,5 @@ def index():
 #def init_model():
 
 if __name__ == '__main__':
-    """# Import dataset"""
-    df = pd.read_csv(DATA_PATH)
-    
-    """# Preprocessing"""
-    
-    #Remove all row with missing bmi_c (bmi_c == -99)
-    df = df.drop(df[df['bmi_c']==-99.0].index, )
-
-    #Duplicate sample with assess_c == 5 
-    df= df.append(df[df['assess_c']==5].iloc[0:2,] )
-    df= df.append(df[df['assess_c']==5].iloc[0:2,] )
-
-    #Split feature to target
-    X = df.drop(['assess_c'],axis=1)
-    y = df['assess_c']
-
-    #Remove irrelevant columns
-    cols_to_drop = {'cancer_c','compfilm_c','CaTypeO','ptid'}
-    X.drop(columns=cols_to_drop, inplace=True)
-
-    # Numerical features
-    numerical_features = ['age_c','bmi_c']
-
-    # Categorical features for X
-    categorical_features = [col for col in X.columns if (col not in numerical_features)]
-
-    # Categorical features index
-    categorical_features_index = [X.columns.to_list().index(categorical_features[i]) for i in range(len(categorical_features)) ]
-
-    # Dataset is very unbalanced : use [SMOTE] with categorical variables
-    oversample = SMOTENC(categorical_features=categorical_features_index)
-    col_tmp = X.columns
-    X, y = oversample.fit_resample(X, y)
-
-    X = pd.DataFrame(X, columns=col_tmp)
-
-    for feature in categorical_features:
-        X[feature] = X[feature].astype('category')
-
-    #Save min and max of numerical variables
-    #global min_age
-    min_age = X['age_c'].min()
-    
-    #global max_age 
-    max_age = X['age_c'].max()
-
-    #global min_bmi
-    min_bmi = X['bmi_c'].max()
-
-    #global max_bmi
-    max_bmi = X['bmi_c'].min() 
-
-    # Preprocessing for numerical features : Normalize same as Standard Scaler
-    for feature in numerical_features:
-        #X[feature] = (X[feature] - np.min(X[feature])) / (np.max(X[feature]) - np.min(X[feature]))
-	X[feature] = X[feature] / 100.0
-
-    # Preprocessing for categorical features : same as OneHotEncoder
-    X = pd.get_dummies(X)
-
-    # Split training samples and test samples
-    x_train, x_test, y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state=0)
-    
-    """## Random Forest"""
-    #global model
-    model = RandomForestClassifier(n_estimators=1000, random_state=1)
-   
-    #global model
-    model.fit(x_train,y_train)
-    #model.fit(X,y)
+    model =  load('model/mammography.joblib')
     app.run(debug=True, use_reloader=False)
